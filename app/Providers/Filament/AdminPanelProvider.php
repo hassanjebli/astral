4a184271bd9c77e25ff6430fae8c55f32b078557
Hassan AIT JABLI <hassanjebli2002@gmail.com>
2026-01-30 13:27:23 +0100

    feat: add language toggle

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use CraftForge\FilamentLanguageSwitcher\FilamentLanguageSwitcherPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -10,6 +11,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -69,6 +71,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])->plugins([
+                FilamentLanguageSwitcherPlugin::make()
+                    ->locales(config('locales'))
+                    ->renderHook(PanelsRenderHook::GLOBAL_SEARCH_BEFORE),
             ]);
     }
 }
---
+++ config/locales.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    [
+        'code' => 'en',
+        'label' => 'English',
+        'flag' => 'us',
+    ],
+
+    [
+        'code' => 'fr',
+        'label' => 'Français',
+        'flag' => 'fr',
+    ],
+
+    [
+        'code' => 'ar',
+        'label' => 'العربية',
+        'flag' => 'ma',
+    ],
+
+];
